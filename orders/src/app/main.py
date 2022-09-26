e1fced71c3940b2472d6e84b36b9700243957c15
bad-logic <45297269+bad-logic@users.noreply.github.com>
2022-09-26 15:25:54 +0545

    adds database setup

--- orders/src/app/main.py
+++ orders/src/app/main.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI
+from db import DBConnector
+from .orders import order_router
 
 request_handler = FastAPI(title="Orders Service")
+
+API_PREFIX = '/orders'
+
+connector = DBConnector()
+
+
+@request_handler.on_event("startup")
+def startup_operations():
+    connector.connect()
+
+
+@request_handler.on_event("shutdown")
+def cleanup_operations():
+    connector.dispose_connection()
+
+
+request_handler.include_router(order_router, prefix=API_PREFIX + "/order")
 
 
 @request_handler.get("/source", status_code=200)
---
+++ orders/src/db/connect.py
@@ -0,0 +1,55 @@
+import os
+from sqlalchemy import create_engine
+from utils import Logger, Singleton
+
+
+logger = Logger().get_logger()
+
+DSN_CONSTANT = "{}+mysqlconnector://{}:{}@{}:{}/{}"
+
+
+class DBConnector(metaclass=Singleton):
+    """create a singleton connection pool with the database 
+
+        Args:
+            metaclass (_type, optional): _description_. Defaults to Singleton
+    """
+
+    def __init__(self) -> None:
+        """Initialize"""
+        user_name = os.getenv("DB_USERNAME")
+        password = os.getenv("DB_PASSWORD")
+        host = os.getenv("DB_HOST")
+        port = os.getenv("DB_PORT")
+        dbname = os.getenv("DB_NAME")
+        dialect = os.getenv("DB_DIALECT")
+
+        self.engine = None
+        self.dsn = DSN_CONSTANT.format(
+            dialect, user_name, password, host, port, dbname)
+
+    def connect(self):
+        """"create a connection pool with the database"""
+        try:
+            logger.debug("Creating a DB engine")
+            if self.engine is None:
+                self.engine = create_engine(
+                    self.dsn, pool_size=100, max_overflow=0)
+            logger.info("Connected to sql server ✔️")
+        except Exception as ex:
+            logger.debug(f"Failed to create a DB engine, {str(ex)}")
+
+    def get_engine(self):
+        """Get the database engine.
+        Returns:
+            Engine: Database engine.
+        """
+        return self.engine
+
+    def dispose_connection(self):
+        """Close the Database connection pool."""
+        self.engine.dispose(True)
+
+    def get_connection_string(self):
+        """returns the connection string to the database"""
+        return self.dsn

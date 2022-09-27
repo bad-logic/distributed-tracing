8d4f9994a9261aab6a032483234a953281a9bad3
bad-logic <mailme.roshanbhattarai@gmail.com>
2022-09-27 17:56:53 +0545

    fixes orm issues with dict and enum

--- orders/src/app/orders/order_model.py
+++ orders/src/app/orders/order_model.py
@@ -1,37 +1,52 @@
+from enum import Enum
+from typing import List
 from pydantic import BaseModel
 from sqlalchemy import Column, Integer, String, JSON, Enum as SqlColumnEnum
 from sqlalchemy.orm import declarative_base
-from typing import Optional
-from enum import Enum
-from typing import List
 
 Base = declarative_base()
 
 
 class StatusEnum(Enum):
-    placed = 'ORDER_PLACED'
-    onRoute = 'ORDER_ON_ROUTE'
-    delivered = 'ORDER_DELIVERED'
+    ORDER_PLACED = 'ORDER_PLACED'
+    ORDER_ON_ROUTE = 'ORDER_ON_ROUTE'
+    ORDER_DELIVERED = 'ORDER_DELIVERED'
 
 
-class OrderModel(BaseModel):
-    """Represents a order model for the orders API's"""
-    id: Optional[int]
+class CreateOrderInterface(BaseModel):
+    """Represents a interface for creating orders"""
     user: int
     product: List[int]
-    status: StatusEnum = StatusEnum.placed
+    status: StatusEnum = StatusEnum.ORDER_PLACED
+    address: str
+
+
+class GetOrderInterface(BaseModel):
+    """Represents a interface for fetching orders"""
+    id: int
+    user: int
+    product: List[int]
+    status: StatusEnum
     address: str
 
 
 class OrderTable(Base):
-    """Represents a order model for the orders Table."""
+    """Represents a order schema for the orders Table."""
 
     __tablename__ = "order"
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     user = Column(Integer, nullable=False)
     product = Column(JSON, nullable=False, default=[])
-    status = Column(SqlColumnEnum(StatusEnum), default=StatusEnum.placed.value)
+    status = Column(SqlColumnEnum(StatusEnum),
+                    default=StatusEnum.ORDER_PLACED.value)
     address = Column(String(50), nullable=False)
 
-    def __repr__(self):
-        return f"Order(id={self.id!r}, user={self.user!r}, product={self.product!r}, status={self.status}, address={self.address})"
+    def dict(self):
+        """ returns the dictionary object for the order table """
+        return {
+            "id": self.id,
+            "user": self.user,
+            "product": self.product,
+            "status": self.status.name,
+            "address": self.address
+        }
--- orders/src/app/orders/order_routes.py
+++ orders/src/app/orders/order_routes.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, HTTPException, status
-from utils import Logger
-from .order_model import OrderModel, StatusEnum
-from .order_controller import OrderController
-import traceback
 from typing import List
+import traceback
+from fastapi import APIRouter, HTTPException, status as status_code
+from utils import Logger
+from .order_model import CreateOrderInterface, GetOrderInterface, StatusEnum
+from .order_controller import OrderController
 
 order_router = APIRouter()
 
@@ -12,78 +12,96 @@ orderController = OrderController()
 logger = Logger().get_logger()
 
 
-@order_router.post("/", status_code=201)
-def create_order(order: OrderModel):
+@order_router.post("/", status_code=status_code.HTTP_201_CREATED, response_model=GetOrderInterface)
+def create_order(order: CreateOrderInterface):
+    """ API to create an order """
     try:
         return orderController.handle_order_creation(order)
-    except Exception:
-        logger.exception(traceback.format_exc())
+    except HTTPException:
+        raise
+    except Exception as general:
         raise HTTPException(
-            status.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to create order"
-        )
+            status_code.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to create order"
+        ) from general
 
 
-@order_router.get("/", status_code=200)
+@order_router.get("/", status_code=status_code.HTTP_200_OK, response_model=List[GetOrderInterface])
 def fetch_orders(offset: int = 1, limit: int = 10):
+    """ API for fetching the list of orders """
     try:
         return orderController.handle_orders_fetch(offset=offset, limit=limit)
-    except Exception:
+    except Exception as ex:
         logger.exception(traceback.format_exc())
         raise HTTPException(
-            status.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to fetch orders"
-        )
+            status_code.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to fetch orders"
+        ) from ex
 
 
-@order_router.get("/{id}", status_code=200)
-def fetch_order(id: int):
+@order_router.get("/{order_id}", status_code=status_code.HTTP_200_OK, response_model=GetOrderInterface)
+def fetch_order(order_id: int):
+    """ API for fetching an order by id """
     try:
-        return orderController.handle_order_fetch(id)
-    except Exception:
+        return orderController.handle_order_fetch(order_id=order_id)
+    except HTTPException:
+        raise
+    except Exception as ex:
         logger.exception(traceback.format_exc())
         raise HTTPException(
-            status.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to fetch order"
-        )
+            status_code.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to fetch order"
+        ) from ex
 
 
-@order_router.patch("/{id}/address", status_code=200)
-def patch_order_address(id: int, address: str):
+@order_router.patch("/{order_id}/address", status_code=status_code.HTTP_200_OK, response_model=GetOrderInterface)
+def patch_order_address(order_id: int, address: str):
+    """ API for updating the address of an order """
     try:
-        return orderController.handle_order_address_patch(id=id, address=address)
-    except Exception:
+        return orderController.handle_order_address_patch(order_id=order_id, address=address)
+    except HTTPException:
+        raise
+    except Exception as ex:
         logger.exception(traceback.format_exc())
         raise HTTPException(
-            status.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to update order address"
-        )
+            status_code.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to update order address"
+        ) from ex
 
 
-@order_router.patch("/{id}/product", status_code=200)
-def patch_order_product(id: int, product: List[int]):
+@order_router.patch("/{order_id}/product", status_code=status_code.HTTP_200_OK, response_model=GetOrderInterface)
+def patch_order_product(order_id: int, product: List[int]):
+    """ API for updating the products of an order """
     try:
-        return orderController.handle_order_product_patch(id=id, product=product)
-    except Exception:
+        return orderController.handle_order_product_patch(order_id=order_id, product=product)
+    except HTTPException:
+        raise
+    except Exception as ex:
         logger.exception(traceback.format_exc())
         raise HTTPException(
-            status.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to update order products"
-        )
+            status_code.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to update order products"
+        ) from ex
 
 
-@order_router.patch("/{id}/status", status_code=200)
-def patch_order_status(id: int, status: StatusEnum):
+@order_router.patch("/{order_id}/status", status_code=status_code.HTTP_200_OK, response_model=GetOrderInterface)
+def patch_order_status(order_id: int, status: StatusEnum):
+    """ API for updating the status of an order """
     try:
-        return orderController.handle_order_status_patch(id=id, status=status)
-    except Exception:
+        return orderController.handle_order_status_patch(order_id=order_id, status=status)
+    except HTTPException:
+        raise
+    except Exception as ex:
         logger.exception(traceback.format_exc())
         raise HTTPException(
-            status.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to update order status"
-        )
+            status_code.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to update order status"
+        ) from ex
 
 
-@order_router.delete("/{id}", status_code=200)
-def delete_order(id: int):
+@order_router.delete("/{order_id}", status_code=status_code.HTTP_204_NO_CONTENT)
+def delete_order(order_id: int):
+    """ API for deleting an order """
     try:
-        return orderController.handle_order_delete(id=id)
-    except Exception:
+        return orderController.handle_order_delete(order_id=order_id)
+    except HTTPException:
+        raise
+    except Exception as ex:
         logger.exception(traceback.format_exc())
         raise HTTPException(
-            status.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to delete order"
-        )
+            status_code.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to delete order"
+        ) from ex

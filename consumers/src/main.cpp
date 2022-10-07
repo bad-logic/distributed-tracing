8a0ebee8598cde8573babb213c48ec62793929b7
bad-logic <mailme.roshanbhattarai@gmail.com>
2022-10-07 09:26:34 +0545

    adds cppkafka for consuming messages in consumer service

--- consumers/src/main.cpp
+++ consumers/src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <cppkafka/cppkafka.h>
+#include "lib/read_yaml"
+#include "lib/notify_consumers"
 
 int main()
 {
+    const char *kafka_brokers = std::getenv("KAFKA_BROKERS");
+    ListenToKafkaTopicsAndNotifyTheConsumers(getKafkaTopicAndItsListeners(), kafka_brokers);
     return 0;
 }

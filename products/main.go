6ef2d5739e51ea4eecb7d4856bd2498d5c5ed7f0
bad-logic <mailme.roshanbhattarai@gmail.com>
2022-10-31 18:35:56 +0545

    seperated middlewares

--- products/main.go
+++ products/main.go
@@ -10,48 +10,14 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"go.opentelemetry.io/otel"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	"go.opentelemetry.io/otel/attribute"
 	"products/db"
 	"products/controllers/product"
 	"products/controllers/app"
 	"products/kafka/client"
 	"products/utils/otlp/telemetry"
+	mw "products/middlewares"
 )
 
-func setTelemetryContext(n httprouter.Handle) httprouter.Handle{
-	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params){	
-
-		// starting with r.Context allows us to make sure to include the traceid and tracestate if the request
-		// is made by any other service that has propagated the traceid or tracestatue
-		ctx := r.Context()
-
-		// create and store a span in the provided context
-		newCtx , span := otel.Tracer(telementaryUtils.SERVICE_NAME).Start(ctx, fmt.Sprintf("%s:%s", r.Method, r.URL))
-		span.SetAttributes(
-			attribute.String("http.method", r.Method),
-			attribute.String("http.route", r.URL.Path),
-		)
-		defer span.End()
-
-		// saving the context with the created span as a part of the request object
-		r = r.WithContext(newCtx)
-		n(w,r,ps)
-	}
-}
-
-
-func panicHandler(n httprouter.Handle) httprouter.Handle{
-	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params){
-	  defer func(){
-		if err:= recover();err!=nil{
-		  w.WriteHeader(http.StatusInternalServerError);
-		  fmt.Println("panic averted",err)
-		  return;
-		}
-	  }()
-	  n(w,r,ps)
-	}
-}
 
 
 func init(){
@@ -91,11 +57,11 @@ func main(){
 	
 	router := httprouter.New()
   
-	router.GET("/products", setTelemetryContext(panicHandler(productController.GetAllProductHandler)))
-	router.POST("/product", setTelemetryContext(panicHandler(productController.CreateProductHandler)))
-	router.GET("/product/:productId", setTelemetryContext(panicHandler(productController.GetProductByIdHandler)))
-	router.PUT("/product/:productId", setTelemetryContext(panicHandler(productController.UpdateProductByIdHandler)))
-	router.DELETE("/product/:productId", setTelemetryContext(panicHandler(productController.DeleteProductByIdHandler)))
+	router.GET("/products", mw.TelemetryContextHandler(mw.PanicHandler(productController.GetAllProductHandler)))
+	router.POST("/product", mw.TelemetryContextHandler(mw.PanicHandler(productController.CreateProductHandler)))
+	router.GET("/product/:productId", mw.TelemetryContextHandler(mw.PanicHandler(productController.GetProductByIdHandler)))
+	router.PUT("/product/:productId", mw.TelemetryContextHandler(mw.PanicHandler(productController.UpdateProductByIdHandler)))
+	router.DELETE("/product/:productId", mw.TelemetryContextHandler(mw.PanicHandler(productController.DeleteProductByIdHandler)))
 
 	router.NotFound = http.HandlerFunc(appController.Global404Handler)
 

ab71b728eff8574edbaea36c8750521d9d55a8e7
bad-logic <mailme.roshanbhattarai@gmail.com>
2022-11-01 22:33:45 +0545

    added telemetry logs

--- orders/src/app/controllers/order_controller.py
+++ orders/src/app/controllers/order_controller.py
@@ -3,13 +3,13 @@
 """
 
 from typing import List
-import traceback
 
+from opentelemetry import trace
 from fastapi import HTTPException, status as status_code
 
 from ..interfaces import CreateOrderInterface, GetOrderInterface, StatusEnum
 from ..services import OrderService
-from utils import Logger
+from utils import TelemetryLogger, SERVICE_NAME, LogLevelEnum
 
 
 class OrderController:
@@ -19,86 +19,127 @@ class OrderController:
     """
 
     def __init__(self):
-        self.logger = Logger().get_logger()
+        self.otlp_logger = TelemetryLogger()
         self.order_service = OrderService()
+        self.tracer = trace.get_tracer(SERVICE_NAME)
 
     def handle_order_creation(self, order: CreateOrderInterface) -> GetOrderInterface:
         """ controller to create an order """
-        try:
-            return self.order_service.create_order(order)
-        except HTTPException:
-            raise
-        except Exception as ex:
-            raise HTTPException(
-                status_code.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to create order"
-            ) from ex
+        with self.tracer.start_as_current_span("OrderController.handle_order_creation") as curr_span:
+            try:
+                order = self.order_service.create_order(order)
+                self.otlp_logger.log(curr_span,
+                                     f"order {order['Id']} created successfully")
+                return order
+            except HTTPException:
+                raise
+            except Exception as ex:
+                self.otlp_logger.error(curr_span, ex, LogLevelEnum.CRITICAL,
+                                       f"response ended with {status_code.HTTP_500_INTERNAL_SERVER_ERROR} status code")
+                raise HTTPException(
+                    status_code.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to create order"
+                ) from ex
 
     def handle_orders_fetch(self, offset: int = 1, limit: int = 10) -> List[GetOrderInterface]:
         """ controller to fetch orders """
-        try:
-            return self.order_service.get_orders(offset=offset, limit=limit)
-        except Exception as ex:
-            self.logger.exception(traceback.format_exc())
-            raise HTTPException(
-                status_code.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to fetch orders"
-            ) from ex
+        with self.tracer.start_as_current_span("OrderController.handle_orders_fetch") as curr_span:
+            try:
+                orders = self.order_service.get_orders(
+                    offset=offset, limit=limit)
+                self.otlp_logger.log(
+                    curr_span, "orders fetched successfully")
+                return orders
+            except Exception as ex:
+                self.otlp_logger.error(curr_span, ex, LogLevelEnum.CRITICAL,
+                                       f"response ended with {status_code.HTTP_500_INTERNAL_SERVER_ERROR} status code")
+                raise HTTPException(
+                    status_code.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to fetch orders"
+                ) from ex
 
     def handle_order_fetch(self, order_id: int) -> GetOrderInterface:
         """ controller to fetch an order """
-        try:
-            return self.order_service.get_order(order_id)
-        except HTTPException:
-            raise
-        except Exception as ex:
-            self.logger.exception(traceback.format_exc())
-            raise HTTPException(
-                status_code.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to fetch order"
-            ) from ex
+        with self.tracer.start_as_current_span("OrderController.handle_order_fetch") as curr_span:
+            try:
+                order = self.order_service.get_order(order_id)
+                self.otlp_logger.log(
+                    curr_span, f"order {order_id} fetched successfully")
+                return order
+            except HTTPException:
+                raise
+            except Exception as ex:
+                self.otlp_logger.error(curr_span, ex, LogLevelEnum.CRITICAL,
+                                       f"response ended with {status_code.HTTP_500_INTERNAL_SERVER_ERROR} status code")
+                raise HTTPException(
+                    status_code.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to fetch order"
+                ) from ex
 
     def handle_order_address_patch(self, order_id: int, address: str) -> GetOrderInterface:
         """ controller to update address an order """
-        try:
-            return self.order_service.update_order_address(order_id=order_id, address=address)
-        except HTTPException:
-            raise
-        except Exception as ex:
-            self.logger.exception(traceback.format_exc())
-            raise HTTPException(
-                status_code.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to update order address"
-            ) from ex
+        with self.tracer.start_as_current_span("OrderController.handle_order_address_patch") as curr_span:
+            try:
+                order = self.order_service.update_order_address(
+                    order_id=order_id, address=address)
+                self.otlp_logger.log(
+                    curr_span, f"address patched for order {order_id} successfully")
+                return order
+            except HTTPException:
+                raise
+            except Exception as ex:
+                self.otlp_logger.error(curr_span, ex, LogLevelEnum.CRITICAL,
+                                       f"response ended with {status_code.HTTP_500_INTERNAL_SERVER_ERROR} status code")
+                raise HTTPException(
+                    status_code.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to update order address"
+                ) from ex
 
     def handle_order_product_patch(self, order_id: int, product: List[int]) -> GetOrderInterface:
         """ controller to update product an order """
-        try:
-            return self.order_service.update_order_product(order_id=order_id, product=product)
-        except HTTPException:
-            raise
-        except Exception as ex:
-            self.logger.exception(traceback.format_exc())
-            raise HTTPException(
-                status_code.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to update order products"
-            ) from ex
+        with self.tracer.start_as_current_span("OrderController.handle_order_product_patch") as curr_span:
+            try:
+                order = self.order_service.update_order_product(
+                    order_id=order_id, product=product)
+                self.otlp_logger.log(
+                    curr_span, f"product updated for order {order_id} successfully")
+                return order
+            except HTTPException:
+                raise
+            except Exception as ex:
+                self.otlp_logger.error(curr_span, ex, LogLevelEnum.CRITICAL,
+                                       f"response ended with {status_code.HTTP_500_INTERNAL_SERVER_ERROR} status code")
+                raise HTTPException(
+                    status_code.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to update order products"
+                ) from ex
 
     def handle_order_status_patch(self, order_id: int, status: StatusEnum) -> GetOrderInterface:
         """ controller to update status an order """
-        try:
-            return self.order_service.update_order_status(order_id=order_id, status=status)
-        except HTTPException:
-            raise
-        except Exception as ex:
-            self.logger.exception(traceback.format_exc())
-            raise HTTPException(
-                status_code.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to update order status"
-            ) from ex
+        with self.tracer.start_as_current_span("OrderController.handle_order_status_patch") as curr_span:
+            try:
+                order = self.order_service.update_order_status(
+                    order_id=order_id, status=status)
+                self.otlp_logger.log(
+                    curr_span, f"status patched for order {order_id} successfully")
+                return order
+            except HTTPException:
+                raise
+            except Exception as ex:
+                self.otlp_logger.error(curr_span, ex, LogLevelEnum.CRITICAL,
+                                       f"response ended with {status_code.HTTP_500_INTERNAL_SERVER_ERROR} status code")
+                raise HTTPException(
+                    status_code.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to update order status"
+                ) from ex
 
     def handle_order_delete(self, order_id: int) -> str:
         """ controller to delete an order """
-        try:
-            return self.order_service.delete_order(order_id)
-        except HTTPException:
-            raise
-        except Exception as ex:
-            self.logger.exception(traceback.format_exc())
-            raise HTTPException(
-                status_code.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to delete order"
-            ) from ex
+        with self.tracer.start_as_current_span("OrderController.handle_order_address_patch") as curr_span:
+            try:
+                self.order_service.delete_order(order_id)
+                self.otlp_logger.log(
+                    curr_span, f"order {order_id} deleted successfully")
+                return f"order {order_id} deleted successfully"
+            except HTTPException:
+                raise
+            except Exception as ex:
+                self.otlp_logger.error(curr_span, ex, LogLevelEnum.CRITICAL,
+                                       f"response ended with {status_code.HTTP_500_INTERNAL_SERVER_ERROR} status code")
+                raise HTTPException(
+                    status_code.HTTP_500_INTERNAL_SERVER_ERROR, "Failed to delete order"
+                ) from ex
--- orders/src/app/services/order_service.py
+++ orders/src/app/services/order_service.py
@@ -7,9 +7,10 @@
 from typing import List
 from fastapi import HTTPException, status as status_code
 from sqlalchemy.orm import sessionmaker, Session
+from opentelemetry import trace
 
 from db import DBConnector
-from utils import Logger
+from utils import TelemetryLogger, SERVICE_NAME
 from ..interfaces import CreateOrderInterface, GetOrderInterface, StatusEnum
 from ..models import OrderTable
 
@@ -26,88 +27,120 @@ class OrderService:
     """
 
     def __init__(self) -> None:
-        self.logger = Logger().get_logger()
+        self.otlp_logger = TelemetryLogger()
+        self.tracer = trace.get_tracer(SERVICE_NAME)
 
     def create_order(self, order: CreateOrderInterface) -> GetOrderInterface:
         """ service to create an order """
-        with session_maker() as session:
-            new_order = OrderTable(
-                user=order.user,
-                product=order.product,
-                status=order.status.value,
-                address=order.address
-            )
-            session.add(new_order)
-            session.commit()
-            session.refresh(new_order, attribute_names=["id"])
-            new_order = new_order.dict()
-        return new_order
+        with self.tracer.start_as_current_span("OrderService.create_order") as curr_span:
+            self.otlp_logger.log(curr_span,
+                                 f"storing {order} in the database")
+            with session_maker() as session:
+                new_order = OrderTable(
+                    User=order.User,
+                    Product=order.Product,
+                    Status=order.Status.value,
+                    Address=order.Address
+                )
+                session.add(new_order)
+                session.commit()
+                session.refresh(new_order, attribute_names=["Id"])
+                new_order = new_order.dict()
+
+            self.otlp_logger.log(curr_span,
+                                 "order stored in database")
+            return new_order
 
     def get_orders(self, offset: int = 1, limit: int = 10) -> List[GetOrderInterface]:
         """ service to fetch orders """
-        with session_maker() as session:
-            orders = session.query(OrderTable).offset(
-                (offset - 1) * limit).limit(limit).all()
-            orders = [order.dict() for order in orders]
-        return orders
+        with self.tracer.start_as_current_span("OrderService.get_orders") as curr_span:
+            self.otlp_logger.log(curr_span,
+                                 "querying database for order")
+            with session_maker() as session:
+                orders = session.query(OrderTable).offset(
+                    (offset - 1) * limit).limit(limit).all()
+                orders = [order.dict() for order in orders]
+            self.otlp_logger.log(curr_span,
+                                 "orders fetched from database")
+            return orders
 
     def get_order_model_object(self, order_id: int, session: Session) -> GetOrderInterface:
         """ service to fetch an sqlalchemy order object """
-        order = session.query(OrderTable).filter(
-            OrderTable.Id == order_id).first()
-        if order is None:
-            raise HTTPException(
-                status_code=status_code.HTTP_404_NOT_FOUND, detail=f"order with ID {order_id} not found")
-        return order
+        with self.tracer.start_as_current_span("OrderService.get_order_model_object") as curr_span:
+            self.otlp_logger.log(curr_span,
+                                 f"querying database for order {order_id}")
+            order = session.query(OrderTable).filter(
+                OrderTable.Id == order_id).first()
+            if order is None:
+                raise HTTPException(
+                    status_code=status_code.HTTP_404_NOT_FOUND, detail=f"order with ID {order_id} not found")
+            self.otlp_logger.log(curr_span,
+                                 "order fetched from database")
+            return order
 
     def get_order(self, order_id: int) -> GetOrderInterface:
         """ service to fetch an order """
-        with session_maker() as session:
-            order = self.get_order_model_object(order_id, session)
-            order = order.dict()
-        return order
+        with self.tracer.start_as_current_span("OrderService.get_order") as _:
+            with session_maker() as session:
+                order = self.get_order_model_object(order_id, session)
+                order = order.dict()
+            return order
 
     def update_order_status(self, order_id: int, status: StatusEnum) -> GetOrderInterface:
         """ service to update the status of an order """
-        with session_maker() as session:
-            order = self.get_order_model_object(order_id, session)
-            order.status = status.value
-            session.add(order)
-            session.commit()
-            order = order.dict()
-        return order
+        with self.tracer.start_as_current_span("OrderService.update_order_status") as curr_span:
+            self.otlp_logger.log(curr_span,
+                                 f"updating order {order_id} status to {status.value}")
+            with session_maker() as session:
+                order = self.get_order_model_object(order_id, session)
+                order.Status = status.value
+                session.add(order)
+                session.commit()
+                order = order.dict()
+            self.otlp_logger.log(curr_span,
+                                 "saved the updated order status to the database")
+            return order
 
     def update_order_product(self, order_id: int, product: List[int]) -> GetOrderInterface:
         """ service to update the product of an order """
-        with session_maker() as session:
-            order = self.get_order_model_object(order_id, session)
-            if (order.status.value != StatusEnum.ORDER_PLACED.value):
-                raise HTTPException(
-                    status_code.HTTP_403_FORBIDDEN, detail=f"Cannot update product once order status is {order.status.value}"
-                )
-            order.product = product
-            session.add(order)
-            session.commit()
-            order = order.dict()
-        return order
+        with self.tracer.start_as_current_span("OrderService.update_order_product") as curr_span:
+            with session_maker() as session:
+                order = self.get_order_model_object(order_id, session)
+                if (order.Status.value != StatusEnum.ORDER_PLACED.value):
+                    raise HTTPException(
+                        status_code.HTTP_403_FORBIDDEN, detail=f"Cannot update product once order status is {order.status.value}"
+                    )
+                order.Product = product
+                session.add(order)
+                session.commit()
+                order = order.dict()
+            self.otlp_logger.log(curr_span,
+                                 "updated order product to the database")
+            return order
 
     def update_order_address(self, order_id: int, address: str) -> GetOrderInterface:
         """ service to update the address of an order """
-        with session_maker() as session:
-            order = self.get_order_model_object(order_id, session)
-            if (order.status.value != StatusEnum.ORDER_PLACED.value):
-                raise HTTPException(
-                    status_code.HTTP_403_FORBIDDEN, detail=f"Cannot update address once order status is {order.status.value}"
-                )
-            order.address = address
-            session.add(order)
-            session.commit()
-            order = order.dict()
-        return order
+        with self.tracer.start_as_current_span("OrderService.update_order_address") as curr_span:
+            with session_maker() as session:
+                order = self.get_order_model_object(order_id, session)
+                if (order.Status.value != StatusEnum.ORDER_PLACED.value):
+                    raise HTTPException(
+                        status_code.HTTP_403_FORBIDDEN, detail=f"Cannot update address once order status is {order.status.value}"
+                    )
+                order.Address = address
+                session.add(order)
+                session.commit()
+                order = order.dict()
+            self.otlp_logger.log(curr_span,
+                                 "updated order address to the database")
+            return order
 
     def delete_order(self, order_id: int) -> None:
         """ service to delete an order """
-        with session_maker() as session:
-            order = self.get_order_model_object(order_id, session)
-            session.delete(order)
-            session.commit()
+        with self.tracer.start_as_current_span("OrderService.delete_order") as curr_span:
+            with session_maker() as session:
+                order = self.get_order_model_object(order_id, session)
+                session.delete(order)
+                session.commit()
+            self.otlp_logger.log(curr_span,
+                                 f"successfully deleted order {order_id}")

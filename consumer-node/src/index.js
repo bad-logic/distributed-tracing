1652884829d478a21232c18a63745db68e4e7def
bad-logic <45297269+bad-logic@users.noreply.github.com>
2022-11-02 21:36:31 +0545

    added signal handling

--- consumer-node/src/index.js
+++ consumer-node/src/index.js
@@ -2,4 +2,35 @@ import url from "node:url";
 
 if (import.meta.url === url.pathToFileURL(process.argv[1]).href) {
   console.info("running main application");
+
+  const gracefulShutdown = () => {
+    console.warn("Http Server stopped!!!");
+    process.exit(1);
+  };
+
+  // handling termination signals
+
+  process.on("SIGTERM", () => {
+    // user presses ctrl + C
+    console.error("user presses ctrl + c");
+    gracefulShutdown();
+  });
+
+  process.on("SIGINT", () => {
+    // user presses ctrl + D
+    console.error("user presses ctrl + d");
+    gracefulShutdown();
+  });
+
+  // prevent promise rejection exits
+  process.on("unhandledRejection", (reason, promise) => {
+    console.error("unhandledRejection", reason);
+    throw reason;
+  });
+
+  // prevent dirty exit on code-fault crashes
+  process.on("uncaughtException", (error) => {
+    console.error(`Application Crashed  ${error?.stack?.split("\n")}`);
+    gracefulShutdown();
+  });
 }

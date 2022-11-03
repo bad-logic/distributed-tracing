fe960118b13db4b61ef317a343721743d4dcc01b
bad-logic <mailme.roshanbhattarai@gmail.com>
2022-11-03 16:56:42 +0545

    added trace_id from kafka headers to consumer logs

--- consumer-node/src/lib/notifyConsumers.js
+++ consumer-node/src/lib/notifyConsumers.js
@@ -1,8 +1,29 @@
 import kafkajs from "kafkajs";
-import { SpanStatusCode, trace } from "@opentelemetry/api";
+import {
+  SpanStatusCode,
+  trace,
+  ROOT_CONTEXT,
+  propagation,
+  SpanKind,
+} from "@opentelemetry/api";
+import {
+  SemanticAttributes,
+  MessagingOperationValues,
+  MessagingDestinationKindValues,
+} from "@opentelemetry/semantic-conventions";
 
 const { Kafka } = kafkajs;
 const serviceName = process.env["SERVICE_NAME"];
+
+const bufferTextMapGetter = {
+  get(carrier, key) {
+    return carrier?.[key]?.toString();
+  },
+
+  keys(carrier) {
+    return carrier ? Object.keys(carrier) : [];
+  },
+};
 
 export async function ListenToKafkaTopicsAndNotifyTheConsumers(
   groupId,
@@ -25,11 +46,14 @@ export async function ListenToKafkaTopicsAndNotifyTheConsumers(
 
     const tracer = trace.getTracer(serviceName);
     await consumer.run({
-      eachMessage: async ({ topic, partition, message }) => {
+      eachMessage: async (kafkaPayload) => {
+        const { topic, partition, message } = kafkaPayload;
         const { headers, value, key, offset } = message;
+
+        // transparent: {version}-{trace_id}-{span_id}-{trace_flags}
         let traceparent;
         Object.keys(headers).forEach((key) => {
-          if (key.toLocaleLowerCase() === "traceparent") {
+          if (key.toLowerCase() === "traceparent") {
             traceparent = headers[key].toString();
           }
         });
@@ -38,31 +62,56 @@ export async function ListenToKafkaTopicsAndNotifyTheConsumers(
           `[+] Received message with key  ${key}, topic -> ${topic} on Partition ${partition}, offset ${offset} , with payload  ${payload}`
         );
 
-        tracer.startActiveSpan(`${serviceName}`, async (span) => {
-          span.setAttribute("key", key);
-          span.setAttribute("partition", partition);
-          span.setAttribute("offset", offset);
-          span.setAttribute("topic", topic);
+        const propagatedContext = propagation.extract(
+          ROOT_CONTEXT,
+          headers,
+          bufferTextMapGetter
+        );
 
-          try {
-            await sendDataToConsumer(consumers[topic], payload, traceparent);
-            console.log("✔️  Data Sent Successfully !!!");
-            span.addEvent("log", {
-              severity: "info",
-              message: `Successfully exported data to ${consumers[topic]}.`,
-            });
-          } catch (err) {
-            console.log("Error Sending Data !!!", err);
-            span.recordException(err);
-            span.setStatus({ code: SpanStatusCode.ERROR });
-            span.addEvent("log", {
-              severity: "critical",
-              message: `unable to send data to ${consumers[topic]}.`,
-            });
-          } finally {
-            span.end();
-          }
-        });
+        const span = tracer.startSpan(
+          `${serviceName}`,
+          {
+            kind: SpanKind.CONSUMER,
+            attributes: {
+              [SemanticAttributes.MESSAGING_SYSTEM]: "kafka",
+              [SemanticAttributes.MESSAGING_DESTINATION]: topic,
+              [SemanticAttributes.MESSAGING_DESTINATION_KIND]:
+                MessagingDestinationKindValues.TOPIC,
+              [SemanticAttributes.MESSAGING_OPERATION]:
+                MessagingOperationValues.PROCESS,
+              [SemanticAttributes.MESSAGING_KAFKA_MESSAGE_KEY]: key,
+              [SemanticAttributes.MESSAGING_KAFKA_PARTITION]: partition,
+              offset: offset,
+            },
+          },
+          propagatedContext
+        );
+
+        try {
+          span.addEvent("log", {
+            severity: "info",
+            partition,
+            topic,
+            key,
+            message: `Received kafka payload`,
+          });
+          await sendDataToConsumer(consumers[topic], payload, traceparent);
+          console.log("✔️  Data Sent Successfully !!!");
+          span.addEvent("log", {
+            severity: "info",
+            message: `Successfully exported data to ${consumers[topic]}.`,
+          });
+        } catch (err) {
+          console.log("Error Sending Data !!!", err);
+          span.recordException(err);
+          span.setStatus({ code: SpanStatusCode.ERROR });
+          span.addEvent("log", {
+            severity: "critical",
+            message: `unable to send data to ${consumers[topic]}.`,
+          });
+        } finally {
+          span.end();
+        }
       },
     });
 

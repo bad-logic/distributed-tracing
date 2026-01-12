37c19490463ad5d0ec30ddc34d6c799115bacea6
roshan <45297269+bad-logic@users.noreply.github.com>
2026-01-12 00:55:40 -0600

    chore: fix tests

--- test/test.sh
+++ test/test.sh
@@ -1,18 +1,123 @@
+################################################################
+#
+# Author: RB
+# Version 1
+# This script is used for testing open telemetry logs
+#
+################################################################
 
-order="localhost:8083/order/"
-product="localhost:8082/product"
 
 if [ ! -x $(which curl)  ];then
-        echo "Please install curl command to execute the task"
-        exit 1
+    echo "Please install curl command to execute the task"
+    exit 1
 fi
 
-for i in `seq 1 20`;
-do $(which curl) -s -X POST -d  @$PWD/product.json ${product} -H 'Content-Type: application/json';
+echo "creating products..."
+for i in `seq 1 30`;
+do
+  curl -X POST -H "Content-Type: application/json" -d  "{\"name\": \"summer-collection\",\"price\": \"99.99\",\"userId\": \"$((i * 5))\"}" localhost:8082/product;
+  printf "\n";
 done
 
-for i in `seq 1 4`;
-do $(which curl) -s -X POST -d  @$PWD/order.json ${order} -H 'Content-Type: application/json';
+echo "get individual product"
+for i in `seq 1 45`;
+do
+  curl -X GET localhost:8082/product/"$i";
+  printf "\n";
 done
 
+echo "updating short description"
+for i in `seq 1 45`;
+do
+  curl -X PUT -H "Content-Type: application/json" -d  "{\"ShortDesc\": \"this product is very comfortable\"}" localhost:8082/product/"$i";
+  printf "\n";
+done
+
+echo "get individual product after short description update"
+for i in `seq 1 45`;
+do
+  curl -X GET localhost:8082/product/"$i";
+  printf "\n";
+done
+
+echo "updating name"
+for i in `seq 25 45`;
+do
+  curl -X PUT -H "Content-Type: application/json" -d  "{\"name\": \"winter-collection\"}" localhost:8082/product/"$i";
+  printf "\n";
+done
+
+echo "updating price"
+for i in `seq 1 25`;
+do
+  price="$((i * 5)).99"
+  curl -X PUT -H "Content-Type: application/json" -d  "{\"price\": \"$price\"}" localhost:8082/product/"$i";
+  printf "\n";
+done
+
+echo "get individual product after name description update"
+for i in `seq 1 45`;
+do
+  curl -X GET localhost:8082/product/"$i";
+  printf "\n";
+done
+
+echo "getting products"
+for i in `seq 1 3`;
+do
+  curl -X GET localhost:8082/products;
+  printf "\n";
+done
+
+echo "creating orders"
+for i in `seq 1 25`;
+do
+  prod="[$((i + 1)), $((i + 3)) , $((i + 5))]"
+  curl -X POST -H "Content-Type: application/json" -d  "{\"User\": $i,\"Product\": $prod,\"Address\": \"xyz\" }" localhost:8083/order/;
+  printf "\n";
+done
+
+echo "deleting products"
+for i in `seq 10 15`;
+do
+  curl -X DELETE localhost:8082/product/"$i";
+  printf "\n";
+done
+
+echo "updating orders product"
+for i in `seq 1 9`;
+do
+  prod="[$((i + 2)), $((i + 3)) , $((i + 6))]"
+  curl -X PATCH -H "Content-Type: application/json" -d  "{\"Product\": $prod }" localhost:8083/order/"$i"/product;
+  printf "\n";
+done
+
+echo "updating orders status"
+for i in `seq 9 18`;
+do
+  curl -X PATCH -H "Content-Type: application/json" -d  "{\"Status\": \"ORDER_ON_ROUTE\"}" localhost:8083/order/"$i"/status;
+  printf "\n";
+done
+
+echo "updating orders status"
+for i in `seq 18 25`;
+do
+  curl -X PATCH -H "Content-Type: application/json" -d  "{\"Status\": \"ORDER_DELIVERED\"}" localhost:8083/order/"$i"/status;
+  printf "\n";
+done
+
+echo "updating orders address"
+for i in `seq 1 25`;
+do
+  address="$i street, xyz  $((i + 3))$((i + 1))$((i + 2))$((1 + 9))"
+  curl -X PATCH -H "Content-Type: application/json" -d  "{\"Address\": \"$address\" }" localhost:8083/order/"$i"/address;
+  printf "\n";
+done
+
+echo "deleting orders"
+for i in `seq 1 5`;
+do
+  curl -X DELETE  localhost:8083/order/"$i";
+  printf "\n";
+done
 

92faa9ffc310a47f1a7fa1680cd77ab87966d877
roshan <45297269+bad-logic@users.noreply.github.com>
2026-01-12 00:54:24 -0600

    chore: fix run script

--- run.sh
+++ run.sh
@@ -1,67 +1,108 @@
 #!/bin/bash
+################################################################
+#
+# Author: RB
+# Version 1
+# This script is used for running container apps
+#
+################################################################
 
-# docker containers
-# kafka related
-zk=zookeeper1
-kfk=kafka1
-kuis=kafka-ui-service
-# logging related
-jaegr=jaeger-container
-zpkn=zipkin-container
-otlcl=otel-collector-container
-# fltc=fluentd-container
-# application related
-prods=product-service-container
-prodsdb=products-db-container
-ord=order-service-container
-orddb=orders-db-container
-cns=consumer-service-node-container
-# ccs=consumer-service-cpp-container
+usage(){
+  cat << EOF
 
+  Usage: bash run.sh
 
-kf=("zookeeper1" "kafka1" "kafka-ui-service")
-lg=("jaeger-container","zipkin-container","otel-collector-container")
+   Options:
+           --h                         for help
+       Commands:
+           start                       start the applications
+           stop                        stop the applications
+EOF
+}
+
+external_networks=("dstrace_kafka_network" "dstrace_log_network")
+
+create_external_network(){
+  for i in "${external_networks[@]}"
+      do
+         if docker network ls | grep "$i"; then
+              echo "network already exists"
+          else
+              docker network create "$i"
+              echo "✔ $i created"
+        fi
+      done
+}
 
 start_kafka(){
-    docker-compose -f deploy/kafka-compose.yaml up --build 
+    docker compose -f deploy/kafka-compose.yaml up --build -d
+    echo "✔ kafka infrastructure setup complete"
+}
+
+stop_kafka(){
+    docker compose -f deploy/kafka-compose.yaml down
+    echo "✔ kafka infrastructure destroyed"
 }
 
 start_logs(){
-    docker-compose -f deploy/logs-compose.yaml up --build 
+    docker compose -f deploy/logs-compose.yaml up --build -d
+    echo "✔ logging infrastructure setup complete"
+}
+
+stop_logs(){
+    docker compose -f deploy/logs-compose.yaml down
+    echo "✔ logging infrastructure destroyed"
 }
 
 start_app(){
-    docker-compose -f deploy/docker-compose.yaml up --build
+    docker compose -f deploy/docker-compose.yaml up --build -d
+    echo "✔ started application in detached mode"
 }
 
-start_kafka &
-kafka_active=false
-while [ $kafka_active = false ];
-do
-    kafka_active=true
-    for i in "${kf[@]}"
-    do
-        if [ $kafka_active = true ];
-        then
-            kafka_active=$(docker container inspect -f '{{.State.Running}}' $i)
-        fi
-    done
-done
-echo "done with kafkas..."
-start_logs &
-logs_active=false
-while [ $logs_active = false ];
-do
-    logs_active=true
-    for i in "${lg[@]}"
-    do
-        if [ $logs_active == true ];
-        then
-            logs_active=$(docker container inspect -f '{{.State.Running}}' $i)
-        fi
-    done
-    echo $logs_active
-done
-echo "done with logs..."
-start_app &
-wait
+stop_app(){
+    docker compose -f deploy/docker-compose.yaml down
+    echo "✔ application containers stopped"
+}
+
+start(){
+  create_external_network
+  start_kafka
+  start_logs
+  start_app
+}
+
+stop(){
+  stop_app
+  stop_logs
+  stop_kafka
+  echo "✔ stopped the application"
+}
+
+test(){
+  bash test/test.sh
+}
+
+if [ "$1" == "--h" ];
+then
+    usage
+else
+  if [ -n "$1" ];
+      then
+          case $1 in
+          "start")
+              start
+              ;;
+          "stop")
+              stop
+              ;;
+          *)
+              echo "unknown command $1"
+              usage
+              ;;
+          esac
+      else
+          echo "unknown command $1"
+          usage
+      fi
+
+fi
